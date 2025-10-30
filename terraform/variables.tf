de626e8ebddaeb885176b21a5ad20f4945f24fea
NandhanaCH <220701181@rajalakshmi.edu.in>
2025-10-30 09:47:35 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "acr_name" {
+  default = "facultyleaveacr"
+}
+
+variable "aks_name" {
+  default = "FacultyLeaveAKS"
+}
+
+variable "node_count" {
+  default = 1
+}
