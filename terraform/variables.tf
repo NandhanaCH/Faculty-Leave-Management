991843378fcd2c939f30c42e430a83c6eafcad24
NandhanaCH <220701181@rajalakshmi.edu.in>
2025-11-26 20:00:46 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  default = "southindia"
+}
+
+variable "resource_group_name" {
+  default = "facultyLeaveRG"
+}
+
+variable "acr_name" {
+  default = "facultyleaveacr123"
+}
+
+variable "webapp_name" {
+  default = "faculty-leave-plan"
+}
